bb9c74330d22ed0c29fa17de6596e7b1c4b4eedc
sic <sic@INTERNO.INDUMEX.COM>
2020-08-18 15:29:23 -0300

    LOGIN Y LOGAUT

--- js/login_google.js
+++ js/login_google.js
@@ -1,21 +1,23 @@
-  
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-      window.location = "cover.html";
-  }
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
   
+  sessionStorage.setItem("nombre", profile.getName());
   
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-          location.href ="inicio.html"
-    });
-  }
+  window.location.href="cover.html";
   
-  
- 
+}
+
+function signOut() {
+      
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+  window.location.href="inicio.html";
+});
+}
+
+function onLoad() {
+  gapi.load('auth2', function() {
+    gapi.auth2.init();
+  });
+}

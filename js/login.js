2b5af27da79ee145ad30968452d58e41b7e746d9
sic <sic@INTERNO.INDUMEX.COM>
2020-08-20 16:27:23 -0300

    arreglado logaut y login

--- js/login.js
+++ js/login.js
@@ -1,56 +1,21 @@
+const formulario = document.getElementById("form");
+const username = document.getElementById("user");
+const password = document.getElementById("pass");
 
+formulario.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-var form=document.getElementById("form");
-var user_none=document.getElementById("error-1");
-var password_none=document.getElementById("error-2");
+  let users = {
+    user: username.value,
+    pass: password.value,
+  };
 
-form.addEventListener("submit",validate);
- 
-//valida que no estèn los campos vacìos y despliega aviso en el html si lo estàn
-
-function validate(evt){
-    evt.preventDefault();
-    var user= document.getElementById("user");
-    var password= document.getElementById("password");
-
-    if(user.value.trim() == "") {
-        user_none.innerHTML=`<span>Se necesita tu usuario</span>`;
-        user.focus();
-        
-        
-    }else if(password.value.trim() == ""){
-        user_none.innerHTML=``;
-        password_none.innerHTML=`<span>Se necesita una contraseña</span>`;
-        password.focus();
-
-    }else{
-        password_none.innerHTML=``;
-        var username=user.value
-        sessionStorage.setItem("Nombre", username);
-        
-        window.location.href="cover.html";
-        
-        
-    }
-}
-
-
-//Función que escribe el usuario en la barra de navegación
-//Se ejecuta una vez cargada la pagina con los datos almacenados en sessionStorage
-
-function showUserName(){
-    var bienvenida="";
-    var username=sessionStorage.getItem("Nombre");
-    bienvenida=`<h6 class="mb-1" style="color:#FFFFFF;">Bienvenido/a: `+ `<b>`+username+`</b></h6>`
-    document.getElementById("user-welcome").innerHTML=bienvenida;
-    
-}
+  localStorage.setItem("user", users.user);
+  localStorage.setItem("pass", users.pass);
+  location.href = "cover.html";
+});
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    user.focus();
-
-});
-
+document.addEventListener("DOMContentLoaded", function (e) {});
--- js/login_google.js
+++ js/login_google.js
@@ -1,20 +1,21 @@
 function onSignIn(googleUser) {
   // Useful data for your client-side scripts:
   var profile = googleUser.getBasicProfile();
-  
-  sessionStorage.setItem("nombre", profile.getName());
-  
+  localStorage.setItem("user", profile.getName());
+    
   window.location.href="cover.html";
   
 }
 
-function signOut() {
-      
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-  window.location.href="inicio.html";
-});
-}
+  
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      localStorage.clear();
+      console.log('User signed out.');
+      location.href ="inicio.html"
+    });
+  }
 
 function onLoad() {
   gapi.load('auth2', function() {
